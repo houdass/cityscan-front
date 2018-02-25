4ebce1c8786d62a0f961eb5d952c14c68358d781
Houdass Youness <houd.youness@gmail.com>
2018-02-25 14:07:48 +0000

    Remove debugger

--- src/constants/app.config.ts
+++ src/constants/app.config.ts
@@ -1,4 +1,4 @@
 export class AppConfig {
   public static API_ENDPOINT = 'https://cityscan.herokuapp.com/api';
-  //public static API_ENDPOINT = 'http://localhost:8080/api';
+  // public static API_ENDPOINT = 'http://localhost:8080/api';
 }
